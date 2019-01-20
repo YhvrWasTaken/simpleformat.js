a8cde04a37401176cd5ae3fee54e151f5fd4f4d5
Yhvr#0001 <44927736+YhvrWasTaken@users.noreply.github.com>
2019-01-20 16:37:59 -0500

    v1.3 - Fix let2 notation & added let notation

--- simpleformat.js
+++ simpleformat.js
@@ -1,7 +1,7 @@
 /**********
 *simpleformat.js
 *A simple formatting program
-*v1.2
+*v1.3
 **********/
 
 function format(number, type) {
@@ -22,17 +22,22 @@ function format(number, type) {
   } else if (type == 'let2') {
     var letter1 = exponent - 3;
     var letter2 = 0;
-    console.log(let2[letter2] + let2[letter1])
-    while (letter1 > 27) {
+    while (letter1 >= 26) {
       letter1 -= 26;
       letter2 += 1;
-      console.log(let2[letter2] + let2[letter1])
     }
-    console.log(let2[letter2] + let2[letter1])
     return(mantissa.toFixed(2) + let2[letter2] + let2[letter1])
+  } else if (type == 'let') {
+    var letter1 = exponent - 3;
+    var letter2 = -1;
+    while (letter1 >= 26) {
+      letter1 -= 26;
+      letter2 += 1;
+    }
+    if (letter2 >= 0) return(mantissa.toFixed(2) + let2[letter2] + let2[letter1]);
+    return(mantissa.toFixed(2) + let2[letter1]);
   }
 }
-
 
 
 var nor = ['Thousand', 'Million', 'Billion', 'Trillion', 'Quadrillion', 'Quintillion', 'Sextillion', 'Septillion', 'Octillion', 'Nonillion', 'Decillion', 'Undecillion', 'Duodecillion', 'Tredecillion', 'Quattrodecillion', 'Quindecillion', 'Sexdecillion', 'Septendecillion', 'Octodecillion', 'Novemdecillion', 'Vigintillion', 'Unvigintillion', 'Duovigintillion', 'Tresvigintillion', 'Quattuorvigintillion', 'Quinquavigintillion', 'Sesvigintillion', 'Septemvigintillion', 'Octovigintillion', 'Novemvigintillion', 'Trigintillion', 'Untrigintillion', 'Duotrigintillion', 'Trestrigintillion', 'Quattuortrigintillion', 'Quinquatrigintillion', 'Sestrigintillion', 'Septentrigintillion', 'Octotrigintillion', 'Noventrigintillion', 'Quadragintillion', 'Unquadragintillion', 'Duoquadragintillion', 'Trequadragintillion', 'Quattuorquadragintillion', 'Quinquadragintillion', 'Sexquadragintillion', 'Septenquadragintillion', 'Octoquadragintillion', 'Novemquadragintillion', 'Quinquagintillion', 'Unquinquagintillion', 'Duoquinquagintillion', 'Trequinquagintillion', 'Quattuorquinquagintillion', 'Quinquinquagintillion', 'Sexquinquagintillion', 'Septenquinquagintillion', 'Octoquinquagintillion','Novemquinquagintillion','Sexagintillion','Unsexagintillion','Duosexagintillion','tresexagintillion','Quattuorsexagintillion','Quinsexagintillion','Sexsexagintillion','Septensexagintillion','Octosexagintillion','Novemsexagintillion','Septuagintillion','Unseptuagintillion','Duoseptuagintillion','Treseptuagintillion','Quattuorseptuagintillion','Quinseptuagintillion','Sexseptuagintillion','Septenseptuagintillion', 'Octoseptuagintillion', 'Novemseptuagintillion', 'Octogintillion', 'Unoctogintillion', 'Duooctogintillion', 'Treoctogintillion', 'Quattuoroctogintillion', 'Quinoctogintillion', 'Sexoctogintillion', 'Septenoctogintillion', 'Octooctogintillion', 'Novemoctogintillion', 'Nonagintillion', 'Unnonagintillion', 'Duononagintillion', 'Trenonagintillion', 'Quattuornonagintillion', 'Quinnonagintillion', 'Sexnonagintillion', 'Septennonagintillion', 'Octononagintillion', 'Novemnonagintillion', 'Centillion'];
